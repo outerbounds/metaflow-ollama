ff3fcb9627ecf18439a3f77d8e27f721589080d3
Eddie Mattia <eddie@outerbounds.com>
2025-05-26 17:15:24 -0700

    fix pypi publishing

--- setup.py
+++ setup.py
@@ -10,8 +10,18 @@ setup(
     author_email="hello@outerbounds.co",
     long_description_content_type="text/markdown",
     packages=find_namespace_packages(include=["metaflow_extensions.*"]),
-    py_modules=[
-        "metaflow_extensions",
-    ],
+    # Remove py_modules - it conflicts with packages
     install_requires=[],
+    python_requires=">=3.6",  # Add this for clarity
+    classifiers=[  # Add these for better PyPI presentation
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
 )
